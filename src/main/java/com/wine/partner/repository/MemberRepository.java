0705dc77fa3b2051bdfcea4e0fd241a8911bf889
DESKTOP-DU4AR9B\User <leehan0617@naver.com>
2022-02-25 12:49:19 +0900

    add jpa

---
+++ src/main/java/com/wine/partner/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.wine.partner.repository;
+
+import com.wine.partner.model.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+}
